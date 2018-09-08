ba4941f8c9d3150b073234b03017f4d59732b180
Calabrese Christian <christian.calabrese@outlook.com>
2018-09-08 17:41:50 +0200

    Education 100%

--- tr/en_GB.js
+++ tr/en_GB.js
@@ -11,4 +11,10 @@ R.registerLocale('en-GB',
   'educationheader': "Education",
   'unibobachelor': "Bachelor degree in Computer Science and Engineering",
   'unipvmaster': "Master degree in Computer Engineering",
+  'highschool': "Graduation in Computer Science at IISS G. Salvemini Alessano",
+  'comptia': "CompTia Security+ Certification",
+  'efsetc1': "English EFset C1 Certification Dublin",
+  'pet': "English PET Cambridge Certification",
+  'gese': "English Trinity Gese level 9 Certification",
+
 });
